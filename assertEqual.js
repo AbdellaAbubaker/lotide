dd7494b8bd1e045b7041ebc5e454d54cc887840d
AbdellaAbubaker <aabdella.abubaker@mgail.com>
2023-04-18 18:15:01 -0400

    updated

--- assertEqual.js
+++ assertEqual.js
@@ -1,27 +1,15 @@
-const findKey = function (object, callback) {
-    for (const key in object) {
-        if (callback(object[key])){
-            return key 
-        }
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log("Assertion passed: actual === expected")
+    } else {
+        console.log("Assertion passed: actual !== expected")
     }
-    return undefined
-}
 
+   
 
-//  Scenario 1 
-const planet = { 
+};
 
-        "Blue Hill": { stars: 1 },
-        "Akaleri":   { stars: 3 },
-        "noma":      { stars: 2 },
-        "elBulli":   { stars: 3 },
-        "Ora":       { stars: 2 },
-        "Akelarre":  { stars: 3 }
-}
-const results1 = findKey(planet, x => x.stars === 2);
-assertEqual(result1, "noma");
-
-// Scenario 2 
-
-const result2 = findKey(planet, x => x.planet === 4 );
-assertEqual(result2, undefined);
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
--- head.js
+++ head.js
@@ -1,37 +1,15 @@
 // FUNCTION IMPLEMENTATION
-/*const assertEqual = function(actual, expected) {
+const assertEqual = function(actual, expected) {
     if (actual === expected) {
         console.log("Assertion passed: actual === expected")
     } else {
         console.log("Assertion passed: actual !== expected")
     }
 }
-*/
 
-
-const assertEqual = function(head) {
-
-const head1 = [([5,6,7]), 5]
-
-const head2= [["Hello", "Lighthouse", "Labs"], "Hello"]
-
+const head = function(arr) {
+    return arr[0]
 }
-{
-    if (head1[0] !== head2[0]) {
-        console.log("1st element doesnt equal")
-    }
-    if (head[0] === head[0]) {
-        console.log("1st element equal")
-    }
-    if (head.length === 0) {
-        console.log("Atleast one value needed")
-    }
-  
-}
-
-
-
-
 
 // TEST CODE
 assertEqual(head([5,6,7]), 5);
